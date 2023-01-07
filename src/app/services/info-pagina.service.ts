e218c3bd32f6e14daf31097894d88ffd3ecbd3b1
MarionMBC <melchisedec.bustamante@unah.hn>
2023-01-07 01:45:13 -0600

    About

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,8 +9,17 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info:InfoPagina={}
   cargada:boolean=false
+  equipo:any[]= []
 
   constructor(private http: HttpClient) {
+    this.loadInfo();
+    this.loadTeam();
+  }
+
+
+
+
+private loadInfo(){
     //Leer archivo
     this.http.get('assets/data/data-page.json')
     .subscribe((res:InfoPagina) => {
@@ -18,5 +27,13 @@ export class InfoPaginaService {
       this.info = res
       console.log(res)
     })
-  }
+}
+private loadTeam(){
+  this.http.get('https://portfolio-mbc-default-rtdb.firebaseio.com/equipo.json')
+  .subscribe((res:any)=>{
+    this.equipo = res
+    console.log(res)
+  })
+}
+
 }
